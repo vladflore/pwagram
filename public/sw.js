324ee8f6392eb29f16aa70d65f10c59444eab557
Vlad Flore <vlad.flore@virtual7.ro>
2018-06-03 16:14:37 +0200

    prepare for next steps

--- public/sw.js
+++ public/sw.js
@@ -90,7 +90,7 @@ self.addEventListener('fetch', function (event) {
         .then(function (cache) {
           return fetch(event.request)
             .then(function (res) {
-              trimCache(CACHE_DYNAMIC_NAME, 3);
+              //trimCache(CACHE_DYNAMIC_NAME, 3);
               cache.put(event.request, res.clone());
               return res;
             })
@@ -111,7 +111,7 @@ self.addEventListener('fetch', function (event) {
             return fetch(event.request)
               .then(function (res) {
                 return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
-                  trimCache(CACHE_DYNAMIC_NAME, 3);
+                  //trimCache(CACHE_DYNAMIC_NAME, 3);
                   cache.put(event.request.url, res.clone())
                   return res;
                 })
