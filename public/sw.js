f90b7abf95d4453325ea41c10bdbfcf051ded6fb
Vlad Flore <vlad.flore@virtual7.ro>
2018-06-03 15:31:59 +0200

    improved caching strategy

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,20 @@
-var CACHE_STATIC_NAME = 'static-v12';
-var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+const CACHE_STATIC_NAME = 'static-v12';
+const CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
+var STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
 
 self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
@@ -8,20 +23,7 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_STATIC_NAME)
       .then(function (cache) {
         console.log('[Service Worker] Precaching App Shell');
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/offline.html',
-          '/src/js/app.js',
-          '/src/js/feed.js',
-          '/src/js/material.min.js',
-          '/src/css/app.css',
-          '/src/css/feed.css',
-          '/src/images/main-image.jpg',
-          'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-        ]);
+        cache.addAll(STATIC_FILES);
       })
   );
 
@@ -60,6 +62,9 @@ self.addEventListener('fetch', function (event) {
             })
         })
     )
+  } else if (new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(event.request.url)) {
+    //cache only strategy    
+    event.respondWith(caches.match(event.request));
   } else {
     //network fallback
     event.respondWith(
@@ -76,9 +81,12 @@ self.addEventListener('fetch', function (event) {
                 })
               })
               .catch(function (error) {
-                return caches.open(CACHE_STATIC_NAME).then(function (cache) {
-                  return cache.match('/offline.html');
-                })
+                return caches.open(CACHE_STATIC_NAME)
+                  .then(function (cache) {
+                    if (event.request.url.indexOf('/help')) {
+                      return cache.match('/offline.html');
+                    }
+                  })
               });
           }
         })
