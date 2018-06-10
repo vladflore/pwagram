2445b2207efbf1bc0eb4661003954872d1681558
Vlad Flore <vlad.flore@virtual7.ro>
2018-06-10 15:31:55 +0200

    added firebase function to store data in the firebase database

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const cors = require('cors')({ origin: true });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.storePostData = functions.https.onRequest((request, response) => {
+    cors(function (req, res) {
+        admin.database().ref('posts').push({
+            id: request.body.id,
+            title: request.body.title,
+            location: request.body.location,
+            image: request.body.image
+        }).then(function () {
+            response.status(201).json({ message: 'Data stored', id: request.body.id });
+        }).catch(function (error) {
+            response.status(500).json({ error: error });
+        })
+    })
+});
